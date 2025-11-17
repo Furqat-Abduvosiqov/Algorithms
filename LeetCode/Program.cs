bfe1cd01ea7c06b2e775de3a2ac5de48a953a616
Furqat-Abduvosiqov <furqata93@gmail.com>
2025-11-17 10:40:29 +0500

    feat: add problem with removing duplicates

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,3 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using LeetCode;
+
+ExampleUsage.Example();
 Console.WriteLine("Hello, World!");
---
+++ LeetCode/RemoveDuplicatesFromArray.cs
@@ -0,0 +1,38 @@
+﻿namespace LeetCode;
+
+public class RemoveDuplicatesFromArray
+{
+    public int RemoveDuplicates(int[] nums) {
+        
+        if (nums.Length == 0) return 0;
+        
+        int writeIndex = 1;
+        
+        for (int readIndex = 1; readIndex < nums.Length; readIndex++) {
+            if (nums[readIndex] != nums[readIndex - 1]) {
+                nums[writeIndex] = nums[readIndex];
+                writeIndex++;
+            }
+        }
+        
+        return writeIndex;
+    }
+}
+
+public static class ExampleUsage {
+    
+    public static void Example(){
+        RemoveDuplicatesFromArray solution = new RemoveDuplicatesFromArray();
+        
+        int[] nums = [0,0,1,1,1,2,2,3,3,4];
+        
+        int k = solution.RemoveDuplicates(nums);
+        
+        Console.WriteLine("Length of array after removing duplicates: " + k);
+        Console.Write("Array after removing duplicates: ");
+        
+        for (int i = 0; i < k; i++) {
+            Console.Write(nums[i] + " ");
+        }
+    }
+}
